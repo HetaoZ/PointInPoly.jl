0b078ac5484110106e9a037c65568b6990e803db
HetaoZ <iamzhtr@hotmail.com>
2020-11-08 16:13:09 +0800

    deal with `on polygon`

--- README.md
+++ README.md
@@ -5,7 +5,15 @@
 [![Build Status](https://github.com/HetaoZ/PointInPoly.jl/workflows/CI/badge.svg)](https://github.com/HetaoZ/PointInPoly.jl/actions)
 [![Coverage](https://codecov.io/gh/HetaoZ/PointInPoly.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/HetaoZ/PointInPoly.jl)
 
-Check if a point lies in a 2D polygon (available) or a 3D polyhedron (planned, currently not available).
+
+
+Check if a point lies in a 2D polygon (available) or a 3D polyhedron (planned, currently not available). Return 1 if inside, 0 if outside, -1 if exactly on the edge. 
+
+`vertices_x`/`vertices_y`: Vector of `x`/`y` of the polygon vertices. Note that `x[end] != x[1]` (very important).
+ 
+`point_x`/`point_y`: `x`/`y` of the point.
+
+This algorithm was proposed by W. Randolph Franklin: https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html. Extension to "3D point in polyhedron" is still in plan.
 
 # Install
 ```
@@ -16,13 +24,14 @@ Check if a point lies in a 2D polygon (available) or a 3D polyhedron (planned, c
 ```
 using PointInPoly
 
-# rectangle
+# coordinates of polygon vertices, in clockwise or anti-clockwise sequence.
 polyX = [0, 1, 1, 0]
 polyY = [0, 0, 1, 1]
+
+# point
 pointX = 0.5
 pointY = 0.2
 
-# returns 0 if the point is inside, otherwise 1.
-pinpoly(polyX, polyY, pointX, pointY)
+# check
 pinpoly(polyX, polyY, pointX, pointY)
 ```
--- src/PointInPoly.jl
+++ src/PointInPoly.jl
@@ -1,7 +1,8 @@
 module PointInPoly
+export pinpoly 
 
 """
-Find if a point lies within a 2D polygon. Return 0 if it lies inside, else return 1. 
+Check if a point lies inside a 2D polygon. Return 1 if inside, 0 if outside, -1 if exactly on the edge. 
 
 `vertices_x`/`vertices_y`: Vector of `x`/`y` of the polygon vertices. Note that `x[end] != x[1]` (very important).
  
@@ -10,25 +11,35 @@ Find if a point lies within a 2D polygon. Return 0 if it lies inside, else retur
 This algorithm was proposed by W. Randolph Franklin: https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html. Extension to "3D point in polyhedron" is still in plan.
 """  
 function pinpoly(vertices_x::Vector, vertices_y::Vector, point_x::Real, point_y::Real)
-    @assert length(vertices_x) == length(vertices_y)
     nvert = length(vertices_x)
     VertX = [vertices_x; vertices_x[1]]
     VertY = [vertices_y; vertices_y[1]]
+    for i = 1:nvert
+        if pinline(VertX[i], VertY[i], VertX[i+1], VertY[i+1], point_x, point_y)
+            return -1
+        end
+    end
     if ( (point_x < minimum(VertX)) || (point_x > maximum(VertX)) || (point_y < minimum(VertY)) || (point_y > maximum(VertY)) )
-        c = 1
-    else
-        c = 1
-        for i = 1:nvert
-            j = i + 1
-            if ( (VertY[i] > point_y) ⊻ (VertY[j] > point_y ) ) && ( point_x < (VertX[j] - VertX[i]) * (point_y - VertY[i]) / (VertY[j] - VertY[i]) + VertX[i] )
-                # ⊻ : \xor
-                c = 1 - c
-            end
+        return 0
+    end
+    c = 0
+    for i = 1:nvert
+        j = i + 1
+        if ( (VertY[i] > point_y) ⊻ (VertY[j] > point_y ) ) && ( point_x < (VertX[j] - VertX[i]) * (point_y - VertY[i]) / (VertY[j] - VertY[i]) + VertX[i] )
+            # ⊻ : \xor
+            c = 1 - c
         end
     end
     # c = 0: inside, 1: outside
     return c
 end
-export pinpoly  
+ 
+function pinline(X1::Real, Y1::Real, X2::Real, Y2::Real, x::Real, y::Real)
+    if (x - X1) * (Y2 - Y1) == (y - Y1) * (X2 - X1) 
+        return true
+    else
+        return false
+    end
+end
 
 end
