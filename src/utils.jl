f375ed3ea4590747884d525e4b4afce6c5660231
HetaoZ <iamzhtr@hotmail.com>
2022-08-23 10:04:08 +0800

    改进了between

--- src/utils.jl
+++ src/utils.jl
@@ -9,24 +9,10 @@ function cross_product(a::Vector{T} where T <: Real, b::Vector{T} where T <: Rea
     end
 end
 
-function betweeneq(p::NTuple{3,Float64}, point1::NTuple{3,Float64}, point2::NTuple{3,Float64})
-    ans = true
-    for i = 1:3
-        if !(point1[i] <= p[i] <= point2[i])
-            ans = false
-            break
-        end
-    end
-    return ans
+@inline function betweeneq(a::Vector{T}, lo::Vector{T}, hi::Vector{T}) where T <: Real
+    return all(lo .≤ a .≤ hi)
 end
 
-function between(p::NTuple{3,Float64}, point1::NTuple{3,Float64}, point2::NTuple{3,Float64})
-    ans = true
-    for i = 1:3
-        if !(point1[i] < p[i] < point2[i])
-            ans = false
-            break
-        end
-    end
-    return ans
+@inline function between(a::Vector{T}, lo::Vector{T}, hi::Vector{T}) where T <: Real
+    return all(lo .< a .< hi)
 end
