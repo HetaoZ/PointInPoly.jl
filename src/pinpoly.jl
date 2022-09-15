fb50c8a84884860b7e6003efdfcafc49d1d37198
HetaoZ <iamzhtr@hotmail.com>
2022-09-15 17:52:12 +0800

    修改常数

--- src/pinpoly.jl
+++ src/pinpoly.jl
@@ -95,9 +95,9 @@ function ray_intersect_face(X::NTuple{2,Float64}, P1::NTuple{2,Float64}, P2::NTu
     end
 end
 
-const INF = 1.0e14
-const EPS = 1.0e-9
-const RAN = (0.59816024370339371,  0.6196549438024468,  0.4755418207471769)
+const INF = 1.0e10
+const EPS = 1.0e-10
+const RAN = (0.59816024370339371,  0.196549438024468,  0.4755418207471769)
 const BIAS = EPS .* RAN
 const D = INF .* collect(RAN)  # 三维射线的无穷远端
 
