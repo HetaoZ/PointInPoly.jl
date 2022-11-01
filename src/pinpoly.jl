e791781d364dd15725383284da1e913f31777ce0
HetaoZ <iamzhtr@hotmail.com>
2022-11-01 13:22:01 +0800

    move `p` a bit

--- src/pinpoly.jl
+++ src/pinpoly.jl
@@ -109,6 +109,9 @@ end
 
 "[@ref] https://blog.csdn.net/u012138730/article/details/80235813"
 function segment_intersect_face(p, q, a, b, c, normal)
+
+    p[1] += 1e-8
+
     # check y and z coordinates
     y = [a[2],b[2],c[2]]
     z = [a[3],b[3],c[3]]
